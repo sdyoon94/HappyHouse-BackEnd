cb70f38291348c6b60a7b95e2008995cd47f0687
sdyoon94 <sdyoon94@gmail.com>
2022-05-02 12:50:35 +0900

    프로젝트 생성

---
+++ happyhouse/src/main/java/com/ssafy/happyhouse/config/WebMvcConfiguration.java
@@ -0,0 +1,9 @@
+package com.ssafy.happyhouse.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+public class WebMvcConfiguration {
+
+}
---
+++ happyhouse/src/main/java/com/ssafy/happyhouse/model/mapper/HouseMapMapper.java
@@ -0,0 +1,16 @@
+package com.ssafy.happyhouse.model.mapper;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.ssafy.happyhouse.model.HouseInfoDto;
+import com.ssafy.happyhouse.model.SidoGugunCodeDto;
+
+public interface HouseMapMapper {
+
+	List<SidoGugunCodeDto> getSido() throws SQLException;
+	List<SidoGugunCodeDto> getGugunInSido(String sido) throws SQLException;
+	List<HouseInfoDto> getDongInGugun(String gugun) throws SQLException;
+	List<HouseInfoDto> getAptInDong(String dong) throws SQLException;
+	
+}
